10043401414c8853403aed56c34768fb2cb8f1b0
craigdm2 <craigdm2@illinois.edu>
2025-09-08 00:40:40 -0600

    Check if least_fav_num == 0 to avoid div/0 error

--- module_1_part_2_example.py
+++ module_1_part_2_example.py
@@ -5,5 +5,8 @@ fav_num = int(fav_num)
 least_fav_num = input("Input your least favorite number: ")
 least_fav_num = int(least_fav_num)
 
-ratio = fav_num / least_fav_num
-print("The number fav:least fav number ratio is: " + str(ratio))
+if least_fav_num == 0:
+    print("Error: Please pick a number other than 0")
+else:
+    ratio = fav_num / least_fav_num
+    print("The number fav:least fav number ratio is: " + str(ratio))
