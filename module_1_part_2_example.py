26ec7fd264d8b13dcf6fbb94138dd53b3857c4a3
craigdm2 <craigdm2@illinois.edu>
2025-09-08 00:34:04 -0600

    Fixed flake issues

--- module_1_part_2_example.py
+++ module_1_part_2_example.py
@@ -1,9 +1,9 @@
-"""Give an input and multiply it"""
+"""Give an input and multiply it."""
 
-fav_num=input("Input your favorite number: ")
-fav_num=int(fav_num)
-least_fav_num=input("Input your least favorite number: ")
-least_fav_num=int(least_fav_num)
+fav_num = input("Input your favorite number: ")
+fav_num = int(fav_num)
+least_fav_num = input("Input your least favorite number: ")
+least_fav_num = int(least_fav_num)
 
-ratio= fav_num/ least_fav_num
-print("The number fav:least fav number ratio is: "+ str(ratio))
+ratio = fav_num / least_fav_num
+print("The number fav:least fav number ratio is: " + str(ratio))
